cd491a6201ec0c1c75be0ba9dddea3890ca51edc
zainali95 <zainali95@yahoo.com>
2017-05-21 23:25:35 +0500

    commit

--- twitterstream.py
+++ twitterstream.py
@@ -4,13 +4,26 @@ import geocoder
 
 class MyStreamListener(tweepy.StreamListener):
         def on_data(self, raw_data):
+            json_load = json.loads(raw_data)
+            text = json_load['text']
+            if 'RT @' not in text:
+                list=[]
                 json_load = json.loads(raw_data)
                 texts = json_load['text']
-                usr=json_load['user']
-                analysis=textblob.TextBlob(texts).sentiment
-                #print(texts+"::"+str(analysis))
-                print(usr)
+                usr_name=json_load['user']['screen_name']
+                mentions=json_load['entities']['user_mentions']
+                mentioned_users= [ mention["screen_name"] for mention in mentions ]
+                hashs=json_load['entities']['hashtags']
+                hash= [ hash["text"] for hash in hashs ]
+                urls=json_load['entities']['urls']
+                url = [ url["url"] for url in urls ]
+                tweet_retweeted=json_load['retweet_count']
+                tweet_createdat=json_load['created_at']
+                usr_folower=json_load['user']['followers_count']
+                analysis=textblob.TextBlob(texts).sentiment.polarity
 
+                list=[usr_name,text,mentioned_users,hash,url,usr_folower,analysis,tweet_retweeted,tweet_createdat]
+                print(list)
                 return True
 
         def on_status(self, status):
@@ -25,12 +38,20 @@ class insertTweets:
         auth.set_access_token(_access_key, _access_secret)
         self.api = tweepy.API(auth)
 
-    def tweets(self,keyword,location):
+    def tweets(self,keyword,loc):
+            stringx=[]
+            self.keyword=keyword
+            self.location=loc
             myStreamListener = MyStreamListener()
             myStream = tweepy.Stream(auth = self.api.auth, listener=myStreamListener)
-            g = geocoder.google(location)
+            g = geocoder.google(loc)
             location=g.geojson['bbox']
-            x=myStream.filter(locations=[location[0],location[1],location[2],location[3]],languages=["en"],track=[keyword])
-            print x
+            self.stringx=[]
+            x=myStream.filter(locations=[location[0],location[1],location[2],location[3]],languages=["en"],track=[keyword],async=True)
+            time.sleep(10)
+            myStream.disconnect()
+           # return True
+
 
 insertTweets().tweets("car","India")
+
