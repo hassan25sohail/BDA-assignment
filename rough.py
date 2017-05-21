362c65782c1861a95f36c6c0b3de215548bd3330
zainali95 <zainali95@yahoo.com>
2017-05-22 03:11:58 +0500

    commit

--- rough.py
+++ rough.py
@@ -17,3 +17,18 @@ stream = MyStreamer(consumer_key, consumer_secret,
                     access_key, access_secret)
 stream.statuses.filter(track='car',locations=[location.latitude,location.longitude,location.latitude+1,location.longitude-1])
 
+'''
+json_load = json.loads(raw_data)
+            text = json_load['text']
+            if 'RT @' not in text:
+            #https://gist.github.com/hrp/900964
+                usr_folower=json_load['user']['followers_count']
+                usr_name=json['user']['screen_name']
+                tweet_url=json_load['entities']['urls']
+                tweet_hashtags=json_load['entities']['hashtags']
+                tweet_retweeted=json_load['retweet_count']
+                tweet_createdat=json_load['created_at']
+                tweet=text
+                analysis=textblob.TextBlob(tweet).sentiment
+
+'''
